67aa764bb45ac1762c0f28c7f3edc81ec3e656f1
Henry <chunhyungjoon@gmail.com>
2024-01-25 23:18:05 +1100

    ready for heroku & fixing package.json-2

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,15 @@ import customerRoute from './routes/customer-route.js';
 import categoryRoute from './routes/category-route.js';
 import serviceRoute from './routes/service-route.js';
 import bookRoute from './routes/book-route.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 dotenv.config();
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(express.static('build'));
 
