4324f83d040486c7fedc2dcd310a4ab1bcd6fc0e
Henry <chunhyungjoon@gmail.com>
2024-01-26 16:56:53 +1100

    chage APP_PROT to just PORT in server.js

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use(categoryRoute);
 app.use(serviceRoute);
 app.use(bookRoute);
 
-const PORT = process.env.APP_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
